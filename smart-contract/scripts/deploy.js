5da0d7422bf68ee63795aec77b11836200027f80
adam <616177709@qq.com>
2022-05-17 03:17:04 +0800

    fix

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -2,22 +2,22 @@ const main = async () => {
   const dogeFactory = await hre.ethers.getContractFactory('DogeCoin')
   const dogeContract = await dogeFactory.deploy()
   await dogeContract.deployed()
-  console.log(dogeContract.address)
+  console.log('Dogecoin deployed to:', dogeContract.address)
 
   const linkFactory = await hre.ethers.getContractFactory('Link')
   const linkContract = await linkFactory.deploy()
   await linkContract.deployed()
-  console.log(linkContract.address)
+  console.log('LinkToken deployed to:', linkContract.address)
 
   const daiFactory = await hre.ethers.getContractFactory('Dai')
   const daiContract = await daiFactory.deploy()
   await daiContract.deployed()
-  console.log(daiContract.address)
+  console.log('DaiToken deployed to:', daiContract.address)
 
   const usdcFactory = await hre.ethers.getContractFactory('Usdc')
   const usdcContract = await usdcFactory.deploy()
   await usdcContract.deployed()
-  console.log(usdcContract.address)
+  console.log('UsdcToken deployed to:', usdcContract.address)
 }
 
 ;(async () => {
